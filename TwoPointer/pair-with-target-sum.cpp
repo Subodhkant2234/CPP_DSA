0c8364acc70dfaf6331706d432e466c4a1fce344
Subodhkant2234 <subodhkant2234@gmail.com>
2025-02-08 20:21:05 +0530

    pair-with-target-sum
    https://www.designgurus.io/course-play/grokking-the-coding-interview/doc/pair-with-target-sum-easy

---
+++ TwoPointer/pair-with-target-sum.cpp
@@ -0,0 +1,33 @@
+//https://www.designgurus.io/course-play/grokking-the-coding-interview/doc/pair-with-target-sum-easy
+using namespace std;
+
+#include <iostream>
+#include <vector>
+
+class Solution {
+
+  public:
+
+  static vector<int> search(const vector<int> &arr, int targetSum) {
+    // TODO: Write your code here
+    int l = 0, r = arr.size() - 1;
+    vector<int> v(2,-1);
+
+    while(l < r)
+    {
+      if(arr[l] + arr[r] == targetSum){
+        v[0] = l;
+        v[1] = r;
+        return v;
+      }
+      else if(arr[l] + arr[r] > targetSum){
+        r--;
+      } else {
+        l++;
+      }
+    }
+    return v;
+  }
+
+};
+
