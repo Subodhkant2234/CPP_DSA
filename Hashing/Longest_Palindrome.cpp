ece576952ed2b3ef31867a37b94c6d36898833f6
Subodhkant2234 <subodhkant2234@gmail.com>
2025-02-13 23:41:37 +0530

    https://www.designgurus.io/course-play/grokking-the-coding-interview/doc/problem-4-longest-palindromeeasy
    https://www.designgurus.io/course-play/grokking-the-coding-interview/doc/problem-4-longest-palindromeeasy

---
+++ Hashing/Longest_Palindrome.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int longestPalindrome(string s) {        
+        int length = 0;
+        int found_odd = false;
+        // ToDo: Write Your Code Here.
+        unordered_map<char, int>mp;
+        for(char str : s)
+            mp[str]++;
+        
+        for(auto x : mp) {
+            if(x.second %2 == 0)
+                length = length + x.second;
+            else{
+                length += (x.second) -1;
+                found_odd = true;
+            }
+        }
+        if(found_odd)
+            length++;
+        return length;
+    }
+};
+
