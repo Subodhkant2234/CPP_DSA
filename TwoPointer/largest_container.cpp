eebbf4b4201c2bfa1924d7c9e33fd69bda55c2de
Subodhkant2234 <subodhkant2234@gmail.com>
2025-02-14 02:01:17 +0530

    https://www.designgurus.io/course-play/grokking-the-coding-interview/doc/container-with-most-water-medium?courseHome=/course/grokking-the-coding-interview
    https://www.designgurus.io/course-play/grokking-the-coding-interview/doc/container-with-most-water-medium?courseHome=/course/grokking-the-coding-interview

---
+++ TwoPointer/largest_container.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int maxArea = 0;  // To store the maximum area found
+        int res = 0;
+        // ToDo: Write Your Code Here.
+        int left = 0, right = height.size()-1;
+        while(left < right){
+            res = min(height[left], height[right]) * (right-left);
+            maxArea = max(res, maxArea);
+            if(height[left] < height[right])
+                left++;
+            else if( height[left] > height[right])
+                right--;
+            else {
+                left++;
+                right--;
+            }
+        }
+        return maxArea;
+    }
+};
