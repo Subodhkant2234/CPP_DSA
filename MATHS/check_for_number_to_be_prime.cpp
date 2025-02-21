3c7d0f9e3b362ba65ec76ca071d2481be12fbf5d
Subodh K <subodhkant2234@gmail.com>
2025-02-21 20:03:02 +0530

    Create check_for_number_to_be_prime.cpp
    https://www.geeksforgeeks.org/problems/prime-number2314/1

---
+++ MATHS/check_for_number_to_be_prime.cpp
@@ -0,0 +1,22 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool isPrime(int n) {
+        // code here
+        int count = 0;
+        if(n <= 1)
+            return false;
+        for(int i = 2; i*i <= n; i++){
+            if(n%i == 0)
+                count++;
+            if(count > 0)
+                return false;
+        }
+        return true;
+    }
+};
