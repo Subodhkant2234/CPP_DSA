4d93f398144fade5d449145c891beaf32ddcb583
Subodh K <subodhkant2234@gmail.com>
2025-03-02 20:04:04 +0530

    Create invert_binary_tree.cpp
    https://leetcode.com/problems/invert-binary-tree/

---
+++ BFS/invert_binary_tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        queue<TreeNode*> q;
+        if (root == NULL)
+            return root;
+        q.push(root);
+        while (!q.empty()) {
+            TreeNode* Curr = q.front();
+            q.pop();
+            swap(Curr->left, Curr->right);
+            if (Curr->left != NULL)
+                q.push(Curr->left);
+            if (Curr->right != NULL)
+                q.push(Curr->right);
+        }
+        return root;
+    }
+};
