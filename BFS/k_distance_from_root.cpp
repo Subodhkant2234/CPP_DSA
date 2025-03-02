7d535174ca8d8e571912552c8edb57f28e639ba0
Subodh K <subodhkant2234@gmail.com>
2025-03-02 17:03:34 +0530

    Update k_distance_from_root.cpp

--- BFS/k_distance_from_root.cpp
+++ BFS/k_distance_from_root.cpp
@@ -37,3 +37,17 @@ class Solution {
         }
     }
 };
+
+
+/*
+void printKDist(Node *root,int k){
+    if(root==NULL)return;
+    
+    if(k==0){cout<<root->key<<" ";}
+    else{
+    printKDist(root->left,k-1);
+    printKDist(root->right,k-1);
+    }
+}  
+
+*/
