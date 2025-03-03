a2881e68cdf7048ef8cf78a74048cb67593bd4ab
Subodh K <subodhkant2234@gmail.com>
2025-03-03 23:28:22 +0530

    Create print_node_at_k_distance.cpp

---
+++ Binary_Tree/print_node_at_k_distance.cpp
@@ -0,0 +1,13 @@
+void printKDist(Node *root,int k){
+    if(root==NULL)return;
+    
+    if(k==0)
+    {
+      cout<<root->key<<" ";
+    }
+    else
+    {
+    printKDist(root->left,k-1);
+    printKDist(root->right,k-1);
+    }
+}  
