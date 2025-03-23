c77d0b219aa267393670ff007eb5019f6ddf492a
Subodh K <subodhkant2234@gmail.com>
2025-03-24 01:58:46 +0530

    Valid_Anagram.cpp
    https://leetcode.com/problems/valid-anagram/description/

--- strings/valid-anagram.cpp
+++ strings/valid-anagram.cpp
@@ -20,3 +20,24 @@ public:
       return true;
     }
 };
+
+/*
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int count[256] = {0};
+        if (s.length() != t.length())
+            return false;
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s[i]]++;
+            count[t[i]]--;
+        }
+        for (int i = 0; i < 256; i++) {
+            if (count[i] != 0)
+                return false;
+        }
+        return true;
+    }
+};
+*/
