7b2740193bc7ecb7ca37d68f472cddea5fe56d7d
Subodh K <subodhkant2234@gmail.com>
2025-03-24 22:35:20 +0530

    Update sizeof_implement.cpp

--- MATHS/sizeof_implement.cpp
+++ MATHS/sizeof_implement.cpp
@@ -7,3 +7,38 @@ int main() {
     printf("Size of int: %zu bytes\n", my_sizeof(x));
     return 0;
 }
+
+#include <stdio.h>
+
+struct MyStruct {
+    char a;
+    int b;
+    double c;
+};
+
+#define STRUCT_SIZE(s) ((char *)(&s + 1) - (char *)(&s))
+
+int main() {
+    struct MyStruct s;
+    printf("Size of struct: %zu bytes\n", STRUCT_SIZE(s));
+    return 0;
+}
+
+
+#include<stdio.h>
+
+struct student{
+    int roll;
+    char name[100];
+    float marks;
+};
+
+int main(){
+
+  struct student *ptr = 0;
+
+  ptr++;
+  printf("Size of the structure student:  %d",ptr);
+
+  return 0;
+}
