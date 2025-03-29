e7e77d1bde811e74d0ee78bd0da692d938bf5b0b
Subodh K <subodhkant2234@gmail.com>
2025-03-29 23:58:42 +0530

    Create maximum-product-of-two-elements-in-an-array.cpp
    https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/description/

---
+++ Array/maximum-product-of-two-elements-in-an-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int largest = INT_MIN;
+        int second_largest = INT_MIN;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] >= largest) {
+                second_largest = largest;
+                largest = nums[i];
+            } else if (second_largest < nums[i]) {
+                second_largest = nums[i];
+            }
+        }
+        return (largest - 1) * (second_largest - 1);
+    }
+};
