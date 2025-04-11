4f0fa20320b10a052b88923cec57120f2542f893
Subodh K <subodhkant2234@gmail.com>
2025-04-12 00:05:37 +0530

    Update longest_common_prefix.cpp

--- strings/longest_common_prefix.cpp
+++ strings/longest_common_prefix.cpp
@@ -13,3 +13,17 @@ class Solution {
             return ans;
         }
     };
+/*
+      string ans = "";
+        sort(strs.begin(), strs.end());
+        int n = strs.size();
+        string first = strs[0], last = strs[n - 1];
+        for (int i = 0; i < min(first.size(), last.size()); i++) {
+            if (first[i] != last[i]) {
+                return ans;
+            }
+            ans += first[i];
+        }
+        return ans;
+    }
+*/
