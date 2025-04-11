bd7236e75b178f6ba262bf52c6469cd4157309c6
Subodh K <subodhkant2234@gmail.com>
2025-04-11 14:35:05 +0530

    Update reverse_word_in_string.cpp
    Added another method to do that without using extra memory.

--- strings/reverse_word_in_string.cpp
+++ strings/reverse_word_in_string.cpp
@@ -25,3 +25,28 @@ class Solution {
             return result;
         }
     };
+
+/* Another method in place, without taking extra memory */
+  /*
+    int n = s.size();
+    string temp = "";
+    string ans = "";
+    int i = 0;
+    while (i < n) {
+        temp = "";
+        while (s[i] != ' ' && i < n) {
+            temp += s[i];
+            i++;
+        }
+        while (s[i] == ' ' && i < n) {
+            i++;
+        }
+        if (!temp.empty()) {
+            if (ans.empty())
+                ans += temp;
+            else
+                ans = temp + ' ' + ans;
+        }
+    }
+    return ans;
+*/
