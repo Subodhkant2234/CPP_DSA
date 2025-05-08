bd65aa994f30d9204fd70e33bec03a2c665b6f3c
Subodh K <subodhkant2234@gmail.com>
2025-05-08 18:19:42 +0530

    Update dfs_iterative_connected_undirected_graph.cpp
    https://www.geeksforgeeks.org/batch/dsa-4/track/DSASP-Graph/problem/depth-first-traversal-for-a-graph

--- Graph/dfs_iterative_connected_undirected_graph.cpp
+++ Graph/dfs_iterative_connected_undirected_graph.cpp
@@ -1,1 +1,27 @@
-
+class Solution {
+  public:
+    vector<int> dfs(vector<vector<int>>& adj) {
+        // Code here
+        vector<int>res;
+        stack<int> st;
+        int V = adj.size();
+        vector<bool> visited(V, false);
+        int s = 0;
+        st.push(s);
+        
+        while(st.empty() == false){
+            int current = st.top();
+            st.pop();
+            
+            if(!visited[current]){
+                res.push_back(current);
+                visited[current] = true;
+                for(auto it = adj[current].rbegin(); it != adj[current].rend(); it++) {
+                    if(!visited[*it])
+                        st.push(*it);
+                }
+            }
+        }
+        return res;
+    }
+};
