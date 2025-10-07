3bebb17d01a1f57f2b425acc2c1381e82da77191
Subodh K <subodhkant2234@gmail.com>
2025-10-07 23:12:36 +0530

    Create Minimum_Path_Sum_in_grid.cpp
    https://leetcode.com/problems/minimum-path-sum/

---
+++ DP/Minimum_Path_Sum_in_grid.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minSumPathUtil(int i, int j, vector<vector<int>>& matrix,
+                       vector<vector<int>>& dp) {
+        // Base cases
+        if (i == 0 && j == 0)
+            return matrix[0][0]; // If we are at the top-left corner, the
+                                 // minimum path sum is the value at (0, 0)
+        if (i < 0 || j < 0)
+            return 1e9; // If we go out of bounds, return a large value to
+                        // avoid considering this path
+        if (dp[i][j] != -1)
+            return dp[i][j]; // If the result is already computed, return it
+
+        // Calculate the minimum sum path by considering moving up and moving
+        // left
+        int up = matrix[i][j] + minSumPathUtil(i - 1, j, matrix, dp);
+        int left = matrix[i][j] + minSumPathUtil(i, j - 1, matrix, dp);
+
+        // Store the result in the DP table and return it
+        return dp[i][j] = min(up, left);
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> dp(
+            n, vector<int>(m, -1)); // DP table to memoize results
+        return minSumPathUtil(n - 1, m - 1, grid,
+                              dp); // Start from the bottom-right corner
+    }
+};
