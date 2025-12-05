68b03a1734bb85b4657ba99a6e99c962da388b5b
Subodh K <subodhkant2234@gmail.com>
2025-12-05 09:11:05 +0530

    Implement longestNonRepeatingSubstring function

--- sliding window/longest_substring_with_distinct_char.cpp
+++ sliding window/longest_substring_with_distinct_char.cpp
@@ -25,3 +25,26 @@ class Solution {
         return res;
     }
 };
+
+
+
+class Solution {
+   public:
+    int longestNonRepeatingSubstring(string& s) {
+        int windowStart = 0;
+        unordered_map<char, int> mp;
+        int maxLen = 0;
+
+        for (int windowEnd = 0; windowEnd < s.length(); windowEnd++) {
+            char right = s[windowEnd];
+            if (mp.find(right) != mp.end() && mp[right] >= windowStart) {
+                windowStart = mp[right] + 1;
+            }
+
+            mp[right] = windowEnd;
+
+            maxLen = max(maxLen, windowEnd - windowStart + 1);
+        }
+        return maxLen;
+    }
+};
