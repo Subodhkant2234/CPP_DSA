5f5c523a0b33b05a9d9e4b835fbef7c2d47e8d24
Subodh K <subodhkant2234@gmail.com>
2025-12-10 04:19:59 +0530

    Add toggleBits function to toggle bits between L and R

--- QnA/toggle_between_given_range.cpp
+++ QnA/toggle_between_given_range.cpp
@@ -6,3 +6,15 @@ class Solution {
         return (N ^ (mask << (L-1)));
     }
 };
+
+
+class Solution {
+public:
+    int toggleBits(int N, int L, int R) {
+        int width = R - L + 1;            // how many bits to toggle
+        int mask = (1 << width) - 1;      // create mask of width bits (all 1s)
+        mask <<= (L - 1);                 // shift to start at bit L
+        
+        return N ^ mask;                  // toggle bits
+    }
+};
